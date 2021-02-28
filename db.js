b5401d9a3cca322f087e8789acc571f91f53b97e
GW <kowonp@gmail.com>
2021-02-28 22:20:57 +0900

    Update Test DB

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 1,
+    title: "Video01",
+    description: "This is Video01",
+    views: 15,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 100,
+      name: "Park",
+      email: "park@gmail.com",
+    },
+  },
+  {
+    id: 2,
+    title: "Video02",
+    description: "This is Video02",
+    views: 10,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 101,
+      name: "Kim",
+      email: "kim@gmail.com",
+    },
+  },
+  {
+    id: 3,
+    title: "Video03",
+    description: "This is Video03",
+    views: 25,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 103,
+      name: "Lee",
+      email: "lee@gmail.com",
+    },
+  },
+  {
+    id: 4,
+    title: "Video04",
+    description: "This is Video04",
+    views: 20,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 104,
+      name: "Choi",
+      email: "choi@gmail.com",
+    },
+  },
+];
